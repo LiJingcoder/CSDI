2b725f0c0c5e290df187ceac0e8e9a4d92bd1783
LiJingcoder <72537855+LiJingcoder@users.noreply.github.com>
2024-09-15 22:02:40 +0800

    Update diff_models.py

--- diff_models.py
+++ diff_models.py
@@ -5,15 +5,15 @@ import math
 from linear_attention_transformer import LinearAttentionTransformer
 
 
-def get_torch_trans(heads=8, layers=1, channels=64):
+def get_torch_trans(heads=8, layers=1, channels=64):#创建Transformer编码器层的，使用标准的PyTorch Transformer编码器
     encoder_layer = nn.TransformerEncoderLayer(
         d_model=channels, nhead=heads, dim_feedforward=64, activation="gelu"
     )
     return nn.TransformerEncoder(encoder_layer, num_layers=layers)
 
 def get_linear_trans(heads=8,layers=1,channels=64,localheads=0,localwindow=0):
-
-  return LinearAttentionTransformer(
+    #去掉了softmax部分，近似矩阵计算k*Q,提高计算效率
+    return LinearAttentionTransformer(
         dim = channels,
         depth = layers,
         heads = heads,
@@ -24,17 +24,21 @@ def get_linear_trans(heads=8,layers=1,channels=64,localheads=0,localwindow=0):
 
 def Conv1d_with_init(in_channels, out_channels, kernel_size):
     layer = nn.Conv1d(in_channels, out_channels, kernel_size)
-    nn.init.kaiming_normal_(layer.weight)
+    nn.init.kaiming_normal_(layer.weight)#初始化layer层的权重
     return layer
 
-
 class DiffusionEmbedding(nn.Module):
+    #定义了一个用于生成扩散步骤的嵌入向量的模块。它利用正弦和余弦函数生成周期性的位置编码，并通过两个全连接层进行投影
     def __init__(self, num_steps, embedding_dim=128, projection_dim=None):
+        #参数num_steps用来表示扩散步骤的数量
         super().__init__()
         if projection_dim is None:
             projection_dim = embedding_dim
+
+        #注册了一个名为 "embedding" 的缓冲区，用于存储预计算的嵌入向量。
+        #persistent=False 表示这个缓冲区不会被保存到模型的状态字典中（即不会被保存到磁盘）
         self.register_buffer(
-            "embedding",
+            "embedding",#名字就叫embedding，使用的时候直接调用embedding
             self._build_embedding(num_steps, embedding_dim / 2),
             persistent=False,
         )
@@ -42,6 +46,7 @@ class DiffusionEmbedding(nn.Module):
         self.projection2 = nn.Linear(projection_dim, projection_dim)
 
     def forward(self, diffusion_step):
+        #这里的diffusion_step才是随机生成的时间t
         x = self.embedding[diffusion_step]
         x = self.projection1(x)
         x = F.silu(x)
@@ -52,7 +57,7 @@ class DiffusionEmbedding(nn.Module):
     def _build_embedding(self, num_steps, dim=64):
         steps = torch.arange(num_steps).unsqueeze(1)  # (T,1)
         frequencies = 10.0 ** (torch.arange(dim) / (dim - 1) * 4.0).unsqueeze(0)  # (1,dim)
-        table = steps * frequencies  # (T,dim)
+        table = steps * frequencies  # (T,dim)，这里*就表示矩阵乘法
         table = torch.cat([torch.sin(table), torch.cos(table)], dim=1)  # (T,dim*2)
         return table
 
@@ -67,7 +72,7 @@ class diff_CSDI(nn.Module):
             embedding_dim=config["diffusion_embedding_dim"],
         )
 
-        self.input_projection = Conv1d_with_init(inputdim, self.channels, 1)
+        self.input_projection = Conv1d_with_init(inputdim, self.channels, 1)#kernel_size=1
         self.output_projection1 = Conv1d_with_init(self.channels, self.channels, 1)
         self.output_projection2 = Conv1d_with_init(self.channels, 1, 1)
         nn.init.zeros_(self.output_projection2.weight)
@@ -82,30 +87,32 @@ class diff_CSDI(nn.Module):
                     is_linear=config["is_linear"],
                 )
                 for _ in range(config["layers"])
-            ]
+            ] #参数layers=4，即一共有4个ResidualBlock
         )
 
     def forward(self, x, cond_info, diffusion_step):
         B, inputdim, K, L = x.shape
 
         x = x.reshape(B, inputdim, K * L)
-        x = self.input_projection(x)
-        x = F.relu(x)
+        x = self.input_projection(x)#一维卷积改变通道数至channel
+        x = F.relu(x)#激活函数
         x = x.reshape(B, self.channels, K, L)
 
         diffusion_emb = self.diffusion_embedding(diffusion_step)
+        #forward中的函数diffusion_step给该对象的forward函数传参数
 
         skip = []
         for layer in self.residual_layers:
             x, skip_connection = layer(x, cond_info, diffusion_emb)
-            skip.append(skip_connection)
+            skip.append(skip_connection) #用之前创建的skip存储skip_connection块数据
 
         x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
+        #将skip连成一个块后，对第一个维度求和，即(4,B,channel,K*L)->(B,channel,K*L),然后整体除以math.sqrt(len(self.residual_layers))
         x = x.reshape(B, self.channels, K * L)
-        x = self.output_projection1(x)  # (B,channel,K*L)
+        x = self.output_projection1(x)  # (B,channel,K*L)，不改变通道数的一维卷积
         x = F.relu(x)
-        x = self.output_projection2(x)  # (B,1,K*L)
-        x = x.reshape(B, K, L)
+        x = self.output_projection2(x)  # (B,1,K*L)，将通道数降为1的一维卷积
+        x = x.reshape(B, K, L)#将数据还原为(B, K, L)
         return x
 
 
@@ -126,7 +133,7 @@ class ResidualBlock(nn.Module):
             self.feature_layer = get_torch_trans(heads=nheads, layers=1, channels=channels)
 
 
-    def forward_time(self, y, base_shape):
+    def forward_time(self, y, base_shape):#时间注意力提取层
         B, channel, K, L = base_shape
         if L == 1:
             return y
@@ -136,21 +143,27 @@ class ResidualBlock(nn.Module):
             y = self.time_layer(y.permute(0, 2, 1)).permute(0, 2, 1)
         else:
             y = self.time_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
+            #y.permute(2, 0, 1)得到(L,B*K,channel),然后再得到(B*K,channel,L)
         y = y.reshape(B, K, channel, L).permute(0, 2, 1, 3).reshape(B, channel, K * L)
+        #展开回(B, K, channel, L)，然后变成(B,channel,K,L),最后变成(B, channel, K * L)
         return y
 
 
-    def forward_feature(self, y, base_shape):
+    def forward_feature(self, y, base_shape):#特征注意力提取层
         B, channel, K, L = base_shape
         if K == 1:
             return y
         y = y.reshape(B, channel, K, L).permute(0, 3, 1, 2).reshape(B * L, channel, K)
+        #(B, channel, K, L)->(B, L, channel, K)->(B * L, channel, K)
         if self.is_linear:
             y = self.feature_layer(y.permute(0, 2, 1)).permute(0, 2, 1)
         else:
             y = self.feature_layer(y.permute(2, 0, 1)).permute(1, 2, 0)
+            #(K, B * L, channel)->(B*L, channel, K)
         y = y.reshape(B, L, channel, K).permute(0, 2, 3, 1).reshape(B, channel, K * L)
+        #(B, L, channel, K)->(B, channel, K, L)->(B, channel, K * L)
         return y
+
 
     def forward(self, x, cond_info, diffusion_emb):
         B, channel, K, L = x.shape
@@ -158,9 +171,10 @@ class ResidualBlock(nn.Module):
         x = x.reshape(B, channel, K * L)
 
         diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(-1)  # (B,channel,1)
+        #这里直接通过线性层改变通道后，利用广播机制与输入数据x直接相加
         y = x + diffusion_emb
 
-        y = self.forward_time(y, base_shape)
+        y = self.forward_time(y, base_shape)  # (B,channel,K*L)
         y = self.forward_feature(y, base_shape)  # (B,channel,K*L)
         y = self.mid_projection(y)  # (B,2*channel,K*L)
 
@@ -169,12 +183,12 @@ class ResidualBlock(nn.Module):
         cond_info = self.cond_projection(cond_info)  # (B,2*channel,K*L)
         y = y + cond_info
 
-        gate, filter = torch.chunk(y, 2, dim=1)
+        gate, filter = torch.chunk(y, 2, dim=1)#分割数据y为两部分，且dim=1为从channel维度处划分
         y = torch.sigmoid(gate) * torch.tanh(filter)  # (B,channel,K*L)
-        y = self.output_projection(y)
+        y = self.output_projection(y) # (B,2*channel,K*L)
 
-        residual, skip = torch.chunk(y, 2, dim=1)
+        residual, skip = torch.chunk(y, 2, dim=1)#用同样的方法再次划分数据
         x = x.reshape(base_shape)
         residual = residual.reshape(base_shape)
         skip = skip.reshape(base_shape)
-        return (x + residual) / math.sqrt(2.0), skip
+        return (x + residual) / math.sqrt(2.0), skip#返回两个参数，一个为前一个块同输入x的和，另一个为后一个块
